6fce0ca5ffd1a99479f0c2e4ec716be8e5a1e0f7
Muhodari Sage <sagemuho@gmail.com>
2021-07-24 23:17:01 +0200

    added functionality to return weather conditions in any city

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 
 # Create your views here.
@@ -6,7 +8,21 @@ from django.shortcuts import render
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-    else:
-        city=''
+        res = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q' + city + '&appid= api key here').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' +
+                          str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp']) + 'K',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
 
-    return render(request, 'index.html',{'city':city})
+
+        }
+
+    else:
+        data = {}
+
+    return render(request, 'index.html', data)
